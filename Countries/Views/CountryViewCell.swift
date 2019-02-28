e789cddcfbe525fa175158861dfa054566a64653
Vitaly Chupryk <vitaly@softmaxsg.com>
2019-02-28 21:11:23 +0100

    Add CountryViewCell

---
+++ Countries/Views/CountryViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  Copyright © 2019 Vitaly Chupryk. All rights reserved.
+//
+
+import UIKit
+
+final class CountryViewCell: UITableViewCell, ImageContainerView {
+    
+    private lazy var placeholderImage = UIImage(named: "FlagPlaceholder")!
+
+    @IBOutlet weak var countryFlagImage: UIImageView?
+    @IBOutlet weak var nameLabel: UILabel?
+    @IBOutlet weak var populationLabel: UILabel?
+    @IBOutlet weak var areaSizeLabel: UILabel?
+    @IBOutlet weak var areaSizeWrapper: UIView?
+
+    var imageLoader: ImageLoaderProtocol = ImageLoader()
+    var currentImageLoaderTask: Cancellable?
+    
+    // ImageContainerView works with `imageView` property
+    override var imageView: UIImageView? { return countryFlagImage }
+
+    func configure(with viewModel: CountryListItemViewModelProtocol) {
+        nameLabel?.text = viewModel.name
+        populationLabel?.text = viewModel.population
+        areaSizeLabel?.text = viewModel.areaSize
+        areaSizeWrapper?.isHidden = viewModel.areaSize.isEmpty
+        
+        displayImage(with: viewModel.flagUrl, placeholder: placeholderImage)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.cancelImageLoading()
+    }
+    
+}
+
+extension CountryViewCell {
+    
+    class func register(in tableView: UITableView, with identifier: String) {
+        tableView.register(
+            UINib(nibName: "CountryViewCell", bundle: nil),
+            forCellReuseIdentifier: identifier
+        )
+    }
+    
+}
+
---
+++ CountriesTests/Views/CountryViewCellTests.swift
@@ -0,0 +1,68 @@
+//
+//  Copyright © 2019 Vitaly Chupryk. All rights reserved.
+//
+
+import XCTest
+@testable import Countries
+
+final class CountryViewCellTests: XCTestCase {
+    
+    private let defaultWidth: CGFloat = 350
+    
+    private let expectedImageURL = URL(string: "https://flagpedia.net/data/flags/small/aa.png")!
+    private let expectedImage = UIImage.image(size: CGSize(width: 133, height: 80), color: .cyan)
+    
+    private lazy var imageLoaderMock = ImageLoaderMock { url, _, completion in
+        completion(url, url == self.expectedImageURL ? self.expectedImage : nil)
+        return EmptyTask()
+    }
+    
+    private lazy var cell: CountryViewCell = {
+        let cell = view(nibName: "CountryViewCell") as CountryViewCell
+        cell.imageLoader = imageLoaderMock
+        return cell
+    }()
+    
+    func testNormalAppearance() {
+        let cell = self.cell
+        cell.configure(with: CountryListItemViewModel(country: Country(
+            countryCode2: "AA",
+            name: "Country Name",
+            nativeName: "Country Native Name",
+            population: 7_654_321,
+            areaSize: 123_456
+        )))
+        
+        adjustSize(for: cell)
+        snapshotVerifyView(cell)
+    }
+    
+    func testNoAreaSizeAppearance() {
+        let cell = self.cell
+        cell.configure(with: CountryListItemViewModel(country: Country(
+            countryCode2: "AA",
+            name: "Country Name",
+            nativeName: "Country Native Name",
+            population: 7_654_321,
+            areaSize: nil
+        )))
+        
+        adjustSize(for: cell)
+        snapshotVerifyView(cell)
+    }
+
+}
+
+extension CountryViewCellTests {
+    
+    func adjustSize(for view: UIView) {
+        let height = view.systemLayoutSizeFitting(
+            CGSize(width: defaultWidth, height: 0),
+            withHorizontalFittingPriority: .required,
+            verticalFittingPriority: .fittingSizeLevel
+            ).height
+        
+        cell.frame = CGRect(x: 0, y: 0, width: defaultWidth, height: height)
+    }
+    
+}
