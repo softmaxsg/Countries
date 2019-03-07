22f0d785dd790915a30bb8929d2a28897f2a1652
Vitaly Chupryk <vitaly@softmaxsg.com>
2019-03-07 01:02:22 +0100

    Show latest stored current country in the Today widget

--- CountriesTodayWidget/TodayViewController.swift
+++ CountriesTodayWidget/TodayViewController.swift
@@ -7,12 +7,25 @@ import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
+    @IBOutlet weak var countryDetailsWrapper: UIStackView?
+    
+    private let countryDetailsView: CountryDetailsView? = .load()
+    private let countryStorageService = CountryStorageService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+        
+        if let currentCountry = countryStorageService.loadCountry(), let countryDetailsView = countryDetailsView {
+            let countryViewModel = CountryDetailsViewModel(country: currentCountry)
+            countryDetailsView.configure(with: countryViewModel)
+            countryDetailsWrapper?.addArrangedSubview(countryDetailsView)
+        }
     }
 
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
-        
+        countryDetailsView?.isAdvancedDetailsVisible = activeDisplayMode == .expanded
     }
     
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
